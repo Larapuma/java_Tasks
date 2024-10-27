429dc4f6d62cc75630bc1f16e0fba3ed7aa054d4
kaneki4kaken <N.plechkov@yandex.ru>
2024-10-27 22:13:34 +0300

    я думал, что нужно найти самую длинную подстроку в строке 1, которая собирается из строки 2

--- src/LeetcodeTasks/Greates_Commin_Divisor_Of_Strings_1071.java
+++ src/LeetcodeTasks/Greates_Commin_Divisor_Of_Strings_1071.java
@@ -2,7 +2,7 @@ package LeetcodeTasks;
 
 public class Greates_Commin_Divisor_Of_Strings_1071 {
     public static void main(String[] args) {
-        String str1 = "ABCABCada";
+        String str1 = "ABCABC";
         String str2 = "ABC";
         System.out.println(gcdOfStrings(str1,str2));
     }
@@ -11,13 +11,28 @@ public class Greates_Commin_Divisor_Of_Strings_1071 {
         char[] arr_str1 = str1.toCharArray();
         char[] arr_str2 = str2.toCharArray();
         StringBuilder res = new StringBuilder();
-
+        int size = 0;
+        int counter = 0;
 
         for (int i = 0; i < arr_str1.length; i++) {
-            
+            if(size == str2.length() ){
+                size = 0;
 
+                counter+=1;
+
+                if(counter>(int) res.length()/str2.length()){
+                    res.append(str2);
+                }
+            }
+            if(arr_str1[i] == arr_str2[size]) {
+                size += 1;
+            }
+            else{
+                size=0;
+                counter =0;
+            }
         }
-        return "";
+        return res.toString();
 
     }
 
