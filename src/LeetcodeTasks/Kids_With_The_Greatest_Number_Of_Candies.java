821a75c00b602d1235bf5d658a7fe46e53aafc2a
kaneki4kaken <N.plechkov@yandex.ru>
2024-10-27 23:10:35 +0300

    загатовка под новое задание

---
+++ src/LeetcodeTasks/Kids_With_The_Greatest_Number_Of_Candies.java
@@ -0,0 +1,12 @@
+package LeetcodeTasks;
+
+import java.util.List;
+
+public class Kids_With_The_Greatest_Number_Of_Candies {
+    public static void main(String[] args) {
+
+    }
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+
+    }
+}
