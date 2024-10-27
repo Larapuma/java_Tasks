f17a6f5dec9f800d1731e954a2ef76020caa70bc
kaneki4kaken <N.plechkov@yandex.ru>
2024-10-27 22:44:27 +0300

    сделал задание 1071, понял его смысл, нужно было найти НОД, далее вернуть строку длиной НОД

--- src/LeetcodeTasks/Greates_Commin_Divisor_Of_Strings_1071.java
+++ src/LeetcodeTasks/Greates_Commin_Divisor_Of_Strings_1071.java
@@ -5,35 +5,31 @@ public class Greates_Commin_Divisor_Of_Strings_1071 {
         String str1 = "ABCABC";
         String str2 = "ABC";
         System.out.println(gcdOfStrings(str1,str2));
+
     }
     public static String gcdOfStrings(String str1, String str2) {
-
-        char[] arr_str1 = str1.toCharArray();
-        char[] arr_str2 = str2.toCharArray();
+        int str1_size = str1.length();
+        int str2_size = str2.length();
+        char[] arr = str2.toCharArray();
+        String str1_str2 = str1+str2;
+        String str_2_str1 = str2+str1;
+        if (!str1_str2.equals(str_2_str1)){
+        return "";
+        }
+        int size = NOD(str1_size,str2_size);
         StringBuilder res = new StringBuilder();
-        int size = 0;
-        int counter = 0;
-
-        for (int i = 0; i < arr_str1.length; i++) {
-            if(size == str2.length() ){
-                size = 0;
-
-                counter+=1;
-
-                if(counter>(int) res.length()/str2.length()){
-                    res.append(str2);
-                }
-            }
-            if(arr_str1[i] == arr_str2[size]) {
-                size += 1;
-            }
-            else{
-                size=0;
-                counter =0;
-            }
+        for (int i = 0; i < size; i++) {
+            res.append(arr[i]);
         }
         return res.toString();
-
+    }
+    public static int NOD(int x, int y){
+        if(x==y){return x;}
+        while (x!=y){
+        if(x>y)x-=y;
+        else y-=x;
+        }
+        return x;
     }
 
 }
